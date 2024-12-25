49a3726baf801fccfe118f1dc548431f4ef68a57
keto <keto21.abc@gmail.com>
2024-12-26 07:21:57 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "home-keto-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./images/icon.svg"
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  })
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(async function() {
+    try {
+      if (event.request.url.startsWith("http")) {
+        let res = await fetch(event.request);
+        let cache = await cache.open(CACHE_NAME);
+        cache.put(event.request.url, res.clone());
+        return res;
+      }
+      return fetch(event.request);
+    }
+    catch (error) {
+      return cache.match(event.request);
+    }
+  }());
+});
